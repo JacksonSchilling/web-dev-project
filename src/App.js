1be1aa003b61c126e9cbd4041523aa4475074736
Saurabh Gade <saurabhg@saurabhs-air.myfiosgateway.com>
2022-11-23 23:05:39 -0500

    completed login page layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Profile from "./components/profile";
 import EditProfile from "./components/profile/EditProfile";
+import Login from "./components/login";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
             <BrowserRouter>
-                <Link to="/profile">Profile Page</Link>
+                <Link to="/profile">Profile Page | </Link>
+
+                <Link to="/login">Login Page </Link>
                 <div className="container">
                     <Routes>
                         <Route path="/profile" element={<Profile/>}/>
                         <Route path="/edit-profile" element={<EditProfile/>}/>
+                        <Route path="/login" element={<Login/>}/>
                     </Routes>
                 </div>
             </BrowserRouter>
