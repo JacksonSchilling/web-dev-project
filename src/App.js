e93cf7d25fc2ad6ff24ca1f03622994b22cc8900
Saurabh Gade <saurabhg@saurabhs-air.myfiosgateway.com>
2022-11-23 20:50:45 -0500

    completed profile page and edit-profile page layouts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Profile from "./components/profile";
+import EditProfile from "./components/profile/EditProfile";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <h1>Let's begin development!!!</h1>
+            <BrowserRouter>
+                <div className="container">
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/edit-profile" element={<EditProfile/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
     </div>
   );
 }
