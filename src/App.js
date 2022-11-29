73d9ce7bd4766b15fca3825530ce901de76994e2
Jackson Schilling <jackson.schilling99@gmail.com>
2022-11-29 17:50:55 -0500

    details screen

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import Profile from "./components/profile";
 import EditProfile from "./components/profile/EditProfile";
 import Login from "./components/login";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Details from "./components/album_details";
 
 function App() {
   return (
     <div className="App">
             <BrowserRouter>
                 <Link to="/profile">Profile Page | </Link>
-
-                <Link to="/login">Login Page </Link>
+                <Link to="/login">Login Page | </Link>
+                <Link to="/details">Details Page | </Link>
                 <div className="container">
                     <Routes>
                         <Route path="/profile" element={<Profile/>}/>
                         <Route path="/edit-profile" element={<EditProfile/>}/>
                         <Route path="/login" element={<Login/>}/>
+                        <Route path="/details" element={<Details/>}/>
                     </Routes>
                 </div>
             </BrowserRouter>
