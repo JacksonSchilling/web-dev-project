3c808bfd8ddf9e1743621fd88aca0a26f330f0d1
saurabh7998 <saurabhgade7998@gmail.com>
2022-12-11 20:42:58 -0500

    major change in spotify api authentication process -  eliminated the need to login to api everytime on each refresh

--- src/components/useAuth.js
+++ src/components/useAuth.js
@@ -1,46 +1,19 @@
-import { useState, useEffect } from "react"
+import {useState, useEffect} from "react"
 import axios from "axios"
 
 export default function useAuth(code) {
-  const [accessToken, setAccessToken] = useState()
-  const [refreshToken, setRefreshToken] = useState()
-  const [expiresIn, setExpiresIn] = useState()
+    const [accessToken, setAccessToken] = useState()
 
-  useEffect(() => {
-    axios
-        .post("http://localhost:4000/login", {
-          code,
+    useEffect(() => {
+        axios.post("http://localhost:4000/authenticate").then((res) => {
+            setAccessToken(res.data.token)
+            // window.history.push({}, null, "/")
+        }).catch((err) => {
+            console.log(err)
+            // window.location = "/"
         })
-        .then(res => {
-          setAccessToken(res.data.accessToken)
-          setRefreshToken(res.data.refreshToken)
-          setExpiresIn(res.data.expiresIn)
-          window.history.pushState({}, null, "/")
-        })
-        .catch((err) => {
-          console.log(err);
-          window.location = "/"
-        })
-  }, [code])
+    }, [])
 
-  useEffect(() => {
-    if (!refreshToken || !expiresIn) return
-    const interval = setInterval(() => {
-      axios
-          .post("http://localhost:4000/refresh", {
-            refreshToken,
-          })
-          .then(res => {
-            setAccessToken(res.data.accessToken)
-            setExpiresIn(res.data.expiresIn)
-          })
-          .catch(() => {
-            window.location = "/"
-          })
-    }, (expiresIn - 60) * 1000)
 
-    return () => clearInterval(interval)
-  }, [refreshToken, expiresIn])
-
-  return accessToken
+    return accessToken
 }
