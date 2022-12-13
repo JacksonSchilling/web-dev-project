105a05b91cc361b036f686bc29dd93df21bb1d49
saurabh7998 <saurabhgade7998@gmail.com>
2022-12-12 22:14:05 -0500

    restrict access to user-management page to admins only

--- src/components/UserManagement/index.js
+++ src/components/UserManagement/index.js
@@ -1,35 +1,59 @@
 import React, {useEffect} from "react";
-import {deleteUserThunk, findAllUsersThunk, registerThunk} from "../login/users-thunks";
+import {
+    deleteUserThunk,
+    findAllUsersThunk,
+    registerThunk
+} from "../login/users-thunks";
 import {useDispatch, useSelector} from "react-redux";
-import currentUser from "../login/current-user";
 
 const UserManagement = () => {
     const {users} = useSelector((state) => state.users)
+    const {currentUser} = useSelector((state) => state.users)
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(findAllUsersThunk())
     }, [users])
-    return(
-        <>
+    return (
+        <div className="container">
             {
-                currentUser.admin &&
-                <h2>User Management</h2>
+                currentUser?.admin ? (
+                    <div>
+                        <h2>User Management</h2>
+
+                        <ul className="list-group">
+                            {
+                                users.map((user) =>
+                                              <li className="list-group-item"
+                                                  key={user.id}>
+                                                  <i onClick={() => {
+                                                      dispatch(deleteUserThunk(
+                                                          user._id))
+                                                  }}
+                                                     className="bi bi-trash float-end"></i>
+                                                  <span>{user.username} </span>
+                                              </li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                ) : (
+                    <div className="container" style={{width: '18rem'}}>
+                        <div className="card">
+                            <img src="/images/uhOh.png" className="card-img-top" alt=""/>
+                            <div className="card-body">
+                                <p className="card-text text-black text-center pt-2">
+                                    You are not an ADMIN
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    // <div>
+                    //     Hello
+                    // </div>
+                )
             }
-            <ul className="list-group">
-                {
-                    users.map((user) =>
-                        <li className="list-group-item"
-                            key={user.id}>
-                            <i onClick={() => {
-                                dispatch(deleteUserThunk(user._id))
-                            }}
-                               className="bi bi-trash float-end"></i>
-                            <span>{user.username} </span>
-                        </li>
-                    )
-                }
-            </ul>
-        </>
+        </div>
     )
 
 }
